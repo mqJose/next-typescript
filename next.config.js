b1310a9958f0c9d0e6e2a9e5c175671a85663c0c
Jose David <mq.josedavid@gmail.com>
2021-09-14 02:50:12 -0400

    :bookmark: Adding Sass Custom

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
--- src/components/SideBarMenuCardView.tsx
+++ src/components/SideBarMenuCardView.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import { SideBarMenuCard } from "../types/types"
+import { classNames } from "../utils/classes"
 
-const SideBarMenuCardView = (props: any) => {
+interface SideBarMenuCardViewProps {
+  card: SideBarMenuCard;
+  isOpen: boolean;
+}
+
+const SideBarMenuCardView = ({ card, isOpen }: SideBarMenuCardViewProps) => {
   return (
-    <div>
-      SideBarMenuCardView
+    <div className="SideBarMenuCardView">
+      <Image className="profile" alt="Picture of the author" src={card.photoUrl} layout='fill' objectFit='contain'/>
+      <div className={classNames('profileInfo', isOpen ? '' : 'collapsed')}>
+        <div className="name">{card.displayName}</div>
+        <div className="title">{card.displayName}</div>
+        <div className="url">
+          <a href={card.url}>Ir al perfil</a>
+        </div>
+      </div>
     </div>
   )
 }
